a6d1b91915a16afe32fcbc6a0a67048b94db62c8
Yu-Ting Shi <nn6124030@gs.ncku.edu.tw>
2024-03-21 01:03:06 +0800

    Create MultiLayerPerceptron (MLP) networks

--- models/perceptron.py
+++ models/perceptron.py
@@ -1,8 +1,10 @@
+from typing import List
+
 import torch
 import torch.nn as nn
 
 
-__all__ = ['Perceptron']
+__all__ = ['Perceptron', 'MultiLayerPerceptron']
 
 
 class Perceptron(nn.Module):
@@ -16,3 +18,18 @@ class Perceptron(nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = torch.flatten(x, start_dim=1)
         return self.fc(x)
+
+
+class MultiLayerPerceptron(nn.Module):
+    def __init__(self, in_size: List[int], num_classes: int = 10) -> None:
+        super().__init__()
+        in_size.append(num_classes)
+        layers = [
+            Perceptron(in_size[i], in_size[i + 1] ** 2)
+            for i in range(len(in_size) - 1)
+        ]
+        self.layers = nn.Sequential(*layers)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = torch.flatten(x, start_dim=1)
+        return self.layers(x)
